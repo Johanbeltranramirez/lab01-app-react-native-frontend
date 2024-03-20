a12c3d3e65702da8c0a6ce3e487533974144e389
Johanbeltranramirez <mvelazcovelasco17@gmail.com>
2024-03-20 17:43:02 -0500

    (feat): Ya rendericé el botón y los elementos del form, también implementé los paquetes para navegacion entre pantalla

--- app-react-native/App.tsx
+++ app-react-native/App.tsx
@@ -1,29 +1,62 @@
-// Importar las dependencias necesarias
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, ToastAndroid }
+  from 'react-native';
+import { RoundedButton } from './src/components/RoundedButton';
 
 export default function App() {
   return (
-    // Estructura básica de la aplicación
-    <View style={styles.container}>
-      <Image
-      source={ require('./assets/chef.jpg')}
-      style={styles.imageBackground}
-      />
-
-      <View style={styles.logoContainer}>
+      // Estructura básica de la aplicación
+      <View style={styles.container}>
         <Image
-            source={require('./assets/logo.png')}
-            style={styles.logoImage}
+            source={require('./assets/chef.jpg')}
+            style={styles.imageBackground}
+        />
+
+        <View style={styles.logoContainer}>
+          <Image
+              source={require('./assets/logo.png')}
+              style={styles.logoImage}
             />
-        <Text style={styles.logoText}>FOOD APP</Text>
+
+          <Text style={styles.logoText}>FOOD APP</Text>
         </View>
 
-      <View style={styles.form}>
+        <View style={styles.form}>
+          <Text style={styles.formText}>INGRESAR</Text>
+          <View style={styles.formInput}>
+            <Image style={styles.formIcon}
+                   source={require('./assets/email.png')}
+            />
+            <TextInput
+                style={styles.formTextInput}
+                placeholder='Correo electrónico'
+                keyboardType='email-address'
+            />
+          </View>
+
+          <View style={styles.formInput}>
+            <Image style={styles.formIcon}
+                   source={require('./assets/password.png')}
+            />
+            <TextInput
+                style={styles.formTextInput}
+                placeholder='Contraseña'
+                keyboardType='default'
+                secureTextEntry={true}
+            />
+          </View>
+
+          <View style={{ marginTop: 30 }}>
+            <RoundedButton text='ENTRAR' onPress={ () => ToastAndroid.show('HOLA!',
+                ToastAndroid.SHORT)}/>
+          </View>
+
+          <View style={styles.formRegister}>
+            <Text>¿No tienes cuenta?</Text>
+            <Text style={styles.formRegisterText}>Regístrate</Text>
+          </View>
+        </View>
       </View>
-
-    </View>
-
   );
 }
 
@@ -39,16 +72,48 @@ const styles = StyleSheet.create({
     opacity: 0.7,
     bottom: '30%',
   },
-  form:{
+  form: {
     width: '100%',
     height: '40%',
     backgroundColor: 'white',
     position: 'absolute',
     bottom: 0,
-    borderBottomLeftRadius: 40,
-    borderBottomRightRadius: 40,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    padding: 30,
   },
-
+  formText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  formIcon: {
+    width: 25,
+    height: 25,
+    marginTop: 5,
+  },
+  formInput: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  formTextInput: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '#AAAAAA',
+    marginLeft: 15,
+  },
+  formRegister: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  formRegisterText: {
+    fontStyle: 'italic',
+    color: 'orange',
+    borderBottomWidth: 1,
+    borderBottomColor: 'orange',
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
   logoContainer: {
     position: 'absolute',
     alignSelf: 'center',
@@ -64,6 +129,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 10,
     fontWeight: 'bold',
-  }
+  },
 });
-
---
+++ app-react-native/src/views/home/home.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export const HomeScreen = () => {
+    return (
+        <div>Screen</div>
+    )
+}
