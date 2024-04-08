353480e547e6fb072ff224a26f4c53290d8f9cc8
Johanbeltranramirez <johanframirez13@gmail.com>
2024-04-08 02:19:27 -0500

    (backend)creo api con node, añado los controllers

---
+++ Node.js/config/config.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+ host: 'localhost', 
+ user: 'root', 
+ // Mi contraseña kenichi769
+ password: 'root', 
+ database: 'nodejs_base1',
+ authPlugins : {
+    mysql_clear_password: () => Buffer.from('root','utf-8' ),
+    caching_sh2_password : true,
+ }
+});
+db.connect(function(err) {
+ if (err) throw err; 
+ console.log('Base de datos conectada');
+ 
+});
+module.exports = db; 
---
+++ Node.js/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
+/**
+* Importar rutas
+*/
+const users = require('./routes/usersRoutes');
+const port = process.env.PORT || 3000; 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.disable('x-powered-by');
+app.set('port', port);
+/**
+* LLamando las rutas
+*/
+users(app);
+//direccion ip V4 de la maquina, consultar con ipconfig 
+//mi ip:192.168.1.43
+server.listen(3000, '10.175.81.109' || 'localhost', function() {
+ console.log('Aplicación de NodeJS ' + process.pid + ' inicio en el puerto ' + port);
+});
+app.get('/', (req, res) => { 
+ res.send('Ruta raiz del Backend');
+});
+//Error handler 
+app.use((err, req, res, next) => { 
+ console.log(err);
+ res.status(err.status || 500).send(err.stack);
+}); 
