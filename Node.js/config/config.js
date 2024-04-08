e767f2f4740a36d754f9bdc4f79aa4cc2951c9e0
Johanbeltranramirez <johanframirez13@gmail.com>
2024-04-08 02:21:10 -0500

    (backend)creo la bd con Mysql

--- Node.js/config/config.js
+++ Node.js/config/config.js
@@ -1,9 +1,8 @@
 const mysql = require('mysql2');
 const db = mysql.createConnection({
- host: 'localhost', 
- user: 'root', 
- // Mi contraseña kenichi769
- password: 'root', 
+ host: 'localhost',
+ user: 'root',
+ password: 'JFRB1234',
  database: 'nodejs_base1',
  authPlugins : {
     mysql_clear_password: () => Buffer.from('root','utf-8' ),
@@ -11,8 +10,8 @@ const db = mysql.createConnection({
  }
 });
 db.connect(function(err) {
- if (err) throw err; 
+ if (err) throw err;
  console.log('Base de datos conectada');
- 
+
 });
-module.exports = db; 
+module.exports = db;
